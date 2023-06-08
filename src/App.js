6fbc54a8f6460f03e2ffea834edd395211e89555
blacktigerq8 <blacktigerq8@gmail.com>
2023-06-08 21:34:11 +0300

    Completed Theme with Light and Dark Mode Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Router } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
