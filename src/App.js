1067a64acaa6d4546bf5e0e5769008ee6029d339
blacktigerq8 <blacktigerq8@gmail.com>
2023-06-09 18:27:58 +0300

    Completed Topbar, Sidebar, Header, Team, Contacts, and Invoices

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import { Routes, Router } from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/Team";
+import Invoices from "./scenes/Invoices";
+import Contacts from "./scenes/Contacts";
+import Bar from "./scenes/Bar";
+import Form from "./scenes/Form";
+import Line from "./scenes/Line";
+import Pie from "./scenes/Pie";
+import FAQ from "./scenes/Faq";
+import Geography from "./scenes/Geography";
+import Calendar from "./scenes/Calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/team" element={<Team />} />
+              <Route exact path="/invoices" element={<Invoices />} />
+              <Route exact path="/contacts" element={<Contacts />} />
+              <Route exact path="/bar" element={<Bar />} />
+              <Route exact path="/form" element={<Form />} />
+              <Route exact path="/line" element={<Line />} />
+              <Route exact path="/pie" element={<Pie />} />
+              <Route exact path="/faq" element={<FAQ />} />
+              <Route exact path="/geography" element={<Geography />} />
+              <Route exact path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
